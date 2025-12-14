ee2278e3f8cee741d138a0e56f952c792e9c5e91
jjishanbaig <147145605+jjishanbaig@users.noreply.github.com>
2025-12-14 18:11:49 +0530

    Update mongoUrl in keys.js

--- Instagram-mern-stack-main/backend/keys.js
+++ Instagram-mern-stack-main/backend/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoUrl: "mongodb+srv://FlyingPanda730:UgpngUX6DVAx1Xq0@cluster0.ormku05.mongodb.net/?retryWrites=true&w=majority",
+    mongoUrl: "mongodb+srv://FlyingPanda730:UgpngUX6DVAx1Xq0@cluster0",
     Jwt_secret: "faslkfocvneofu"
 
 };
